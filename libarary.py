958b85d3dc4b54795be8686455067c65d1342393
mjyplusone <mjyplusone@qq.com>
2016-03-02 21:21:05 +0800

    library crawler

--- libarary.py
+++ libarary.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import urllib.parse
+import libarary_func
 
 url="http://202.195.144.118:8080/browse/cls_browsing_tree.php"
 s_doctype = "all"
@@ -45,36 +46,47 @@ for i in range(0,22):
 		r.encoding = 'utf-8'
 		soup = BeautifulSoup(r.text,"html.parser")
 		#print (soup.prettify())
+		font = soup.find('font',color='black')
+		if font==None:      #处理只有一页提取不到页数的情况
+			page = 1
+		else:
+			page = int(font.getText())
 
-		book_name = []    #书名
-		book_year = []    #出版日期
-		book_num = []     #借阅次数
+		# book_name = []    #书名
+		# book_year = []    #出版日期
+		# book_num = []     #借阅次数
 
-		for div in soup.find_all('div',class_='list_books'):
-			link = div.find('a')
-
-			book_name.append(link.string)     
-		  
-			year = div.p.getText()
-			pattern = re.compile(r'\d+')
-			if re.findall(pattern, year)==[]:
-				book_year.append('?')
+		for j in range(0,page):
+			if j==0:
+				libarary_func.page("http://202.195.144.118:8080/browse/cls_browsing_book.php?"+"s_doctype="+s_doctype+"&cls="+label[i]+"&clsname="+url_add[i])
 			else:				
-				year = re.findall(pattern, year)[0]
-				book_year.append(re.findall(pattern, year)[0])
+				libarary_func.page("http://202.195.144.118:8080/browse/cls_browsing_book.php?"+"s_doctype="+s_doctype+"&cls="+label[i]+"&clsname="+'&&page='+str(j+1))
+				
+		# for div in soup.find_all('div',class_='list_books'):
+		# 	link = div.find('a')
 
-			url = link.get('href')
-			m = re.search( r"(?<=/).+", url)
-			book_url = "http://202.195.144.118:8080/"+m.group(0)    #详细信息界面
-			r = requests.get(book_url)
-			r.encoding='utf-8'
-			soup2 = BeautifulSoup(r.text,"html.parser")
-			marc = soup2.find("p",id="marc")
-			marc = marc.getText()
-			m = re.findall( r'\d+', marc)    #匹配出浏览次数和借阅次数
-			book_num.append(m[1])            #存储借阅次数
+		# 	book_name.append(link.string)     
+		  
+		# 	year = div.p.getText()
+		# 	pattern = re.compile(r'\d+')
+		# 	if re.findall(pattern, year)==[]:
+		# 		book_year.append('?')
+		# 	else:				
+		# 		year = re.findall(pattern, year)[0]
+		# 		book_year.append(re.findall(pattern, year)[0])
 
-		print(book_name)
-		print(book_year)
-		print(book_num)
+		# 	url = link.get('href')
+		# 	m = re.search( r"(?<=/).+", url)
+		# 	book_url = "http://202.195.144.118:8080/"+m.group(0)    #详细信息界面
+		# 	r = requests.get(book_url)
+		# 	r.encoding='utf-8'
+		# 	soup2 = BeautifulSoup(r.text,"html.parser")
+		# 	marc = soup2.find("p",id="marc")
+		# 	marc = marc.getText()
+		# 	m = re.findall( r'\d+', marc)    #匹配出浏览次数和借阅次数
+		# 	book_num.append(m[1])            #存储借阅次数
+
+		# print(book_name)
+		# print(book_year)
+		# print(book_num)
 
